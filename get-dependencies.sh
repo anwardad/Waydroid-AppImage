6ae01c32559cdb5833e3646376da21559957ded8
cpuxibm <43036847+anwardad@users.noreply.github.com>
2026-07-25 18:00:17 +0000

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -8,14 +8,8 @@ echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm waydroid python-pyclip curl unzip jq
 
-# Install the appropriate image package
-if [ "${DEVEL_RELEASE:-0}" = "1" ]; then
-    echo "Installing GApps images (nightly build)..."
-    pacman -S --noconfirm waydroid-image-gapps || pacman -S --noconfirm waydroid-image
-else
-    echo "Installing vanilla images (stable build)..."
-    pacman -S --noconfirm waydroid-image
-fi
+# The image script is included in the waydroid package
+# No need to install separate image packages
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
@@ -25,9 +19,6 @@ make-aur-package zenity-rs-bin
 make-aur-package 12to11-git
 
 # If the application needs to be manually built that has to be done down here
-
-# if you also have to make nightly releases check for DEVEL_RELEASE = 1
-#
 # if [ "${DEVEL_RELEASE-}" = 1 ]; then
 # 	nightly build steps
 # else
