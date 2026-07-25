d98dafd43369e06e3cbc9aa747874246d0c2d401
cpuxibm <43036847+anwardad@users.noreply.github.com>
2026-07-25 17:27:06 +0000

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -1,27 +1,17 @@
 #!/bin/sh
-
 set -eu
 
 ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm waydroid python-pyclip
+pacman -Syu --noconfirm waydroid python-pyclip curl jq unzip
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
-# Comment this out if you need an AUR package
 make-aur-package zenity-rs-bin
 make-aur-package 12to11-git
 
-# If the application needs to be manually built that has to be done down here
-
-# if you also have to make nightly releases check for DEVEL_RELEASE = 1
-#
-# if [ "${DEVEL_RELEASE-}" = 1 ]; then
-# 	nightly build steps
-# else
-# 	regular build steps
-# fi
+# No further changes needed
